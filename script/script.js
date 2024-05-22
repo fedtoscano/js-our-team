1eb073b2ffc186d80dfadaf5383d5e05b6dc37b1
fedtoscano <federicotoscano611@gmail.com>
2024-05-22 15:48:47 +0200

    mileston 0 fix (commas added)

--- script/script.js
+++ script/script.js
@@ -3,31 +3,31 @@ const team =[
         name: "Wayne Barnett",
         role: "Founder & CEO",
         pic:"wayne-barnett-founder-ceo.jpg"
-    }
+    },
 
     {
         name: "Angela Caroll",
         role: "Chief Editor",
         pic:"angela-caroll-chief-editor.jpg"
-    }
+    },
 
     {
         name: "Walter Gordon",
         role: "Office Manager",
         pic:"walter-gordon-office-manager.jpg"
-    }
+    },
 
     {
         name: "Angela Lopez",
         role: "Social Media Manager",
         pic:"angela-lopez-social-media-manager.jpg"
-    }
+    },
 
     {
         name: "Scott Estrada",
         role: "Developer",
         pic:"scott-estrada-developer.jpg"
-    }
+    },
 
     {
         name: "Barbara Ramos",
